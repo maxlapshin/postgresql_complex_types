9f840f541ae882476911d03a1144dec4f870fc5e
Макс Лапшин <max.lapshin@undev.ru>
2009-05-06 16:38:15 +0400

    Added first README

--- lib/postgres_complex_types.rb
+++ lib/postgres_complex_types.rb
@@ -1,8 +1,9 @@
 begin
+  gem 'RubyInline'
   require 'inline'
   require 'postgres_complex_types/fast_postgres_extractor'
   PostgresComplexTypes::PostgresExtractor = PostgresComplexTypes::FastPostgresExtractor
-rescue Exception => e
+rescue LoadError => e
   puts "#{e.inspect}\n#{e.backtrace.join("\n")}"
   require 'postgres_complex_types/postgres_extractor'
 end
--- test/extract_test.rb
+++ test/extract_test.rb
@@ -1,11 +1,4 @@
-$KCODE='u'
-require 'rubygems'
-require 'test/unit'
-require 'active_support'
-require 'ruby-prof'
-require File.dirname(__FILE__)+'/../lib/postgres_complex_types/fast_postgres_extractor'
-require File.dirname(__FILE__)+'/../lib/postgres_complex_types/postgres_extractor'
-include PostgresComplexTypes
+require File.dirname(__FILE__)+"/test_helper"
 
 class FinderTest < Test::Unit::TestCase
   # include RubyProf::Test
